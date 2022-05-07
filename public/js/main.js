c6a7125a14081c811e168303ce4935e641be82f5
Kimmyli <kc.szul@gmail.com>
2022-05-08 01:44:11 +0200

    Welcome page

--- public/js/main.js
+++ public/js/main.js
@@ -1,20 +1,8 @@
-var dropdowns = document.getElementsByClassName("dropdown");
-var dropdowncontent = document.getElementsByClassName("dropdown-content");
-
-for (let i = 0; i < dropdowns.length; i++) {
-    dropdowns[i].onmouseenter = function () {
-        let width = document.body.clientWidth;
-        let dwidth = dropdowncontent[i].clientWidth;
-        if ((dropdowns[i].offsetLeft + dwidth) > (width - 50)) {
-            dropdowncontent[i].style.right = "1.5rem";
-            dropdowncontent[i].style.left = "auto";
-        } else {
-            dropdowncontent[i].style.right = "auto";
-            dropdowncontent[i].style.left = "1.5rem";
-        }
-
-    }
-}
+document.addEventListener('DOMContentLoaded', function () {
+    var listOfColors = ["color-scheme-1", "color-scheme-2", "color-scheme-3"];
+    var randomNum = Math.floor(Math.random() * listOfColors.length);
+    document.getElementsByClassName("area")[0].classList.add("color-scheme-" + (randomNum + 1));
+});
 
 let shownCategories = [];
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,10 +3,29 @@
 @endsection
 
 @extends('base')
-@section('content')
+@section('body')
+<body>
+    <main class="area">
+        <section class="content">
+            <h1 class="context">Welcome</h1>
 
-    <main>
-        
+            <button onclick="window.location.href='/login'" class="btn btn-primary btn-welcome">Log in</button>
+            <button onclick="window.location.href='/register'" class="btn btn-secondary btn-welcome">Sign up</button>
+        </section>
+
+        <ul class="circles">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
     </main>
-
+</body>
+    
 @endsection
